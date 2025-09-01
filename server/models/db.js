0d434b8ae8691ae86c12ef6286baad0fbe5aa5be
kasuya95 <thunva2004@gmail.com>
2025-09-01 14:26:24 +0700

    new1

--- server/models/db.js
+++ server/models/db.js
@@ -6,6 +6,13 @@ const sequelize = new Sequelize(dbConfig.DB, dbConfig.USER, dbConfig.PASSWORD, {
   port: dbConfig.DBPORT,
   dialect: dbConfig.DIALECT,
   logging: false,
+  omitNull: true,
+  dialectOptions:{
+    ssl:{
+      required: true,
+      rejectUnauthorized:false,
+    }
+  }
 });
 
 testConnection = async () => {
